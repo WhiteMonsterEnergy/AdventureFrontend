131766431301f29e066f89811216102f2d94bdff
Simon <46815527+SimonGotfred@users.noreply.github.com>
2025-10-02 11:48:43 +0200

    initial

---
+++ src/main/java/white/monster/energy/adventurefrontend/AdventureFrontendApplication.java
@@ -0,0 +1,15 @@
+package white.monster.energy.adventurefrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventureFrontendApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(AdventureFrontendApplication.class, args);
+    }
+
+}
