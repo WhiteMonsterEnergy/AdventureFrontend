3611222605cdde32698e97811f69338ed29a8d35
Simon <sigo0001@stud.ek.dk>
2025-10-09 12:27:15 +0200

    ABS-29: navigator

--- src/main/resources/static/javascript/login.js
+++ src/main/resources/static/javascript/login.js
@@ -1,32 +1,8 @@
-// Get the modal
-var modal = document.getElementById("loginPrompt");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var loginForm = document.getElementById("loginForm");
 
-// Get the button that opens the modal
-var btn = document.getElementById("loginBtn");
-
-var form = document.getElementById("loginForm");
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside the modal, close it
-window.onclick = function(event) {
-    if (event.target === modal) {
-        modal.style.display = "none";
-    }
-}
-
-form.onsubmit = async function (){
+var profile;
+loginForm.onsubmit = async function (){
 
     event.preventDefault();
 
@@ -46,11 +22,15 @@ form.onsubmit = async function (){
 
     const response = await fetch("http://localhost:8081/login",fetchOptions);
 
+
+
     if (!response.ok)
     {
         const errorMessage = await response.text();
         throw new Error(errorMessage);
     }
 
-    return response.json();
+    profile = response.json();
+
+    return profile;
 }
