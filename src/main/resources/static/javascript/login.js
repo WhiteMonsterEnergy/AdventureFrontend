d2ee0a530050f76e8c1b0fcf7041762d312908a6
Simon <sigo0001@stud.ek.dk>
2025-10-09 08:33:25 +0200

    ABS-29: login prompt connected to backend

--- src/main/resources/static/javascript/login.js
+++ src/main/resources/static/javascript/login.js
@@ -1,11 +1,13 @@
 // Get the modal
-var modal = document.getElementById("myModal");
+var modal = document.getElementById("loginPrompt");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
 // Get the button that opens the modal
 var btn = document.getElementById("loginBtn");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var form = document.getElementById("loginForm");
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
@@ -17,9 +19,38 @@ span.onclick = function() {
     modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+// When the user clicks anywhere outside the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
+    if (event.target === modal) {
         modal.style.display = "none";
     }
+}
+
+form.onsubmit = async function (){
+
+    event.preventDefault();
+
+    const obj = {
+        "name": "simon",
+        "password": "1234"
+    }
+
+    const objectAsJsonString = JSON.stringify(obj);
+
+    const fetchOptions =
+    {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: objectAsJsonString
+    };
+
+    const response = await fetch("http://localhost:8081/login",fetchOptions);
+
+    if (!response.ok)
+    {
+        const errorMessage = await response.text();
+        throw new Error(errorMessage);
+    }
+
+    return response.json();
 }
